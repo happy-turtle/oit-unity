7a5494082de1ac6cb2a99229a28f53ce7ba653d8
Till Davin <till.davin@gmx.de>
2022-12-17 17:50:58 +0100

    explaining comment

--- URP/Runtime/OrderIndependentTransparencyRenderer.cs
+++ URP/Runtime/OrderIndependentTransparencyRenderer.cs
@@ -18,6 +18,8 @@ namespace OrderIndependentTransparency.URP
 
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
+            // Unity does not provide an example how to perform a fullscreen Blit that works in scene view
+            // Hence only Blit in Game view for now
             if (renderingData.cameraData.cameraType != CameraType.Game)
                 return;
             //Calling ConfigureInput with the ScriptableRenderPassInput.Color argument ensures that the opaque texture is available to the Render Pass
