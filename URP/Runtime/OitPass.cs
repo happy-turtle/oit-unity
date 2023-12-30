5bce1a6f193aed9c00752e1c5b4ff40aea6c23f8
happy-turtle <till.davin@gmx.de>
2023-12-30 14:00:13 +0100

    fix(urp): move cmd clear

--- URP/Runtime/OitPass.cs
+++ URP/Runtime/OitPass.cs
@@ -19,21 +19,21 @@ namespace OrderIndependentTransparency.URP
         private void PreRender(ScriptableRenderContext context, List<Camera> cameras)
         {
             CommandBuffer cmd = CommandBufferPool.Get("Order Independent Transparency Pre Render");
-            cmd.Clear();
             orderIndependentTransparency.PreRender(cmd);
             context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
             CommandBufferPool.Release(cmd);
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             CommandBuffer cmd = CommandBufferPool.Get("Order Independent Transparency");
-            cmd.Clear();
             var mat = orderIndependentTransparency.Render(cmd, renderingData.cameraData.renderer.cameraColorTargetHandle,
                 renderingData.cameraData.renderer.cameraColorTargetHandle);
             Blitter.BlitCameraTexture(cmd, renderingData.cameraData.renderer.cameraColorTargetHandle,
                 renderingData.cameraData.renderer.cameraColorTargetHandle, mat, 0);
             context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
             CommandBufferPool.Release(cmd);
         }
 
